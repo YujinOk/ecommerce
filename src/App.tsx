9797f37a6acf68dd1202e470145776bbf3abba0a
Yujin Ok <yujinok63@gmail.com>
2026-06-15 10:02:02 +0900

    refactor: convert App and index to TypeScript, upgrade to React 18 and use createRoot

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carouselfeature from "./components/Carouselfeature/Carouselfeature";
+import MyNav from "./components/Nav/Nav";
+import Grid from "./components/Grid/Grid";
+import ItemProvider from "./context/ItemContext";
+import Items from "./components/Items/Items";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <ItemProvider>
+        <div>
+          <MyNav />
+          <Routes>
+            <Route path="/buypage" element={
+              <div className="thankyou">
+                Thank you for your purchase!
+              </div>
+            } />
+            <Route path="/items/:id" element={<Items />} />
+            <Route path="/" element={
+              <>
+                <Carouselfeature />
+                <hr className="my-1" />
+                <Grid />
+              </>
+            } />
+          </Routes>
+        </div>
+      </ItemProvider>
+    </Router>
+  );
+};
+
+export default App;
