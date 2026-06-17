32c03d456f31c8e5c54144fee3ac1ba5135ff2bb
Yujin Ok <yujinok63@gmail.com>
2026-06-17 09:03:27 +0900

    feat: add CartProvider and /cart route to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import Carouselfeature from "./components/carousel/Carouselfeature";
 import MyNav from "./components/nav/Nav";
 import Grid from "./components/grid/Grid";
 import ItemProvider from "./context/ItemContext";
+import CartProvider from "./context/CartContext";
 import Items from "./components/items/Items";
+import Cart from "./components/cart/Cart";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -10,24 +12,22 @@ const App = () => {
   return (
     <Router>
       <ItemProvider>
-        <div>
-          <MyNav />
-          <Routes>
-            <Route path="/buypage" element={
-              <div className="thankyou">
-                Thank you for your purchase!
-              </div>
-            } />
-            <Route path="/items/:id" element={<Items />} />
-            <Route path="/" element={
-              <>
-                <Carouselfeature />
-                <hr className="my-1" />
-                <Grid />
-              </>
-            } />
-          </Routes>
-        </div>
+        <CartProvider>
+          <div>
+            <MyNav />
+            <Routes>
+              <Route path="/items/:id" element={<Items />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/" element={
+                <>
+                  <Carouselfeature />
+                  <hr className="my-1" />
+                  <Grid />
+                </>
+              } />
+            </Routes>
+          </div>
+        </CartProvider>
       </ItemProvider>
     </Router>
   );
