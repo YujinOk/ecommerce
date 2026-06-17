fa9dadfb255df9ebb127e7dae7f90feaee100fc6
Yujin Ok <yujinok63@gmail.com>
2026-06-17 09:26:46 +0900

    fix: redesign footer to centered layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Carouselfeature from "./components/carousel/Carouselfeature";
 import MyNav from "./components/nav/Nav";
 import Grid from "./components/grid/Grid";
+import Footer from "./components/footer/Footer";
 import ItemProvider from "./context/ItemContext";
 import CartProvider from "./context/CartContext";
 import Items from "./components/items/Items";
@@ -13,19 +14,22 @@ const App = () => {
     <Router>
       <ItemProvider>
         <CartProvider>
-          <div>
+          <div className="min-h-screen flex flex-col">
             <MyNav />
-            <Routes>
-              <Route path="/items/:id" element={<Items />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/" element={
-                <>
-                  <Carouselfeature />
-                  <hr className="my-1" />
-                  <Grid />
-                </>
-              } />
-            </Routes>
+            <main className="flex-1">
+              <Routes>
+                <Route path="/items/:id" element={<Items />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/" element={
+                  <>
+                    <Carouselfeature />
+                    <hr className="my-1" />
+                    <Grid />
+                  </>
+                } />
+              </Routes>
+            </main>
+            <Footer />
           </div>
         </CartProvider>
       </ItemProvider>
